baa4a5a5ccf179493d2d865864d85d8b4bbab01d
Michael Fisher <mfisher31@gmail.com>
2025-02-15 19:20:32 -0500

    py: limelight: testing out discovery and read.

--- robot.py
+++ robot.py
@@ -6,7 +6,8 @@
 #
 
 import wpilib
-
+import limelight
+import limelightresults
 
 class MyRobot(wpilib.TimedRobot):
     """Main robot class."""
@@ -15,39 +16,44 @@ class MyRobot(wpilib.TimedRobot):
         """Robot-wide initialization code should go here."""
         self.lstick = wpilib.Joystick(0)
         self.motor = wpilib.Talon(3)
-
         self.timer = wpilib.Timer()
         self.loops = 0
+        
+        print("----------- LIMELIGHT -----------")
+        addy = limelight.discover_limelights()[0]
+        self.limelight = ll = limelight.Limelight(addy)
+        result = ll.get_results()
+        print(result)
+        print("-----------")
+        parsed_result = limelightresults.parse_results(result)
+        
+        for result in parsed_result.fiducialResults:
+            print("fiducial_id")
+            print(result.fiducial_id)
+        print("----------- LIMELIGHT -----------")
+
+     
 
     def autonomousInit(self):
-        """Called only at the beginning of autonomous mode."""
         pass
 
     def autonomousPeriodic(self):
-        """Called every 20ms in autonomous mode."""
         pass
 
     def disabledInit(self):
-        """Called only at the beginning of disabled mode."""
         self.logger.info("%d loops / %f seconds", self.loops, self.timer.get())
 
     def disabledPeriodic(self):
-        """Called every 20ms in disabled mode."""
         pass
 
-    def teleopInit(self):
-        """Called only at the beginning of teleoperated mode."""
+    def teleopInit (self):
         self.loops = 0
         self.timer.reset()
         self.timer.start()
 
-    def teleopPeriodic(self):
-        """Called every 20ms in teleoperated mode"""
-        # Move a motor with a Joystick
-        self.motor.set(self.lstick.getY())
-
-        # Print out the number of loop iterations passed every second
-        self.loops += 1
-        if self.timer.advanceIfElapsed(1):
-            self.logger.info("%d loops / second", self.loops)
-            self.loops = 0
+    def teleopPeriodic (self):
+        status = self.limelight.get_status()
+        result = self.limelight.get_results()
+        parsed_result = limelightresults.parse_results(result)
+        for fiducial_result in parsed_result.fiducialResults:
+            print(f"fiducial_id: {fiducial_result.fiducial_id}, cpu: {status['cpu']}")
