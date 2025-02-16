f43be3b34f7666e45389c3d543d57af8e9ba2913
Michael Fisher <mfisher31@gmail.com>
2025-02-15 19:23:41 -0500

    robot.py: update copy notice

--- robot.py
+++ robot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) FIRST and other WPILib contributors.
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
 #
@@ -31,8 +30,6 @@ class MyRobot(wpilib.TimedRobot):
             print("fiducial_id")
             print(result.fiducial_id)
         print("----------- LIMELIGHT -----------")
-
-     
 
     def autonomousInit(self):
         pass
