82f5d266ae53b75099d446a4d67cb7d66051407f
Connor Rey <connorrey1409@gmail.com>
2025-02-21 16:40:27 -0500

    python: regen python code, import to this project. closes #42

--- robot.py
+++ robot.py
@@ -1,60 +1,79 @@
 #!/usr/bin/env python3
 #
+# Copyright (c) FIRST and other WPILib contributors.
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
 #
 
 import wpilib
-import limelight
-import limelightresults
+import commands2
+import typing
 
-class MyRobot(wpilib.TimedRobot):
-    """Main robot class."""
+from robotcontainer import RobotContainer
 
-    def robotInit(self):
-        """Robot-wide initialization code should go here."""
-        self.lstick = wpilib.Joystick(0)
-        self.motor = wpilib.Talon(3)
-        self.timer = wpilib.Timer()
-        self.loops = 0
-        self.limelight = None
-    
-    def limelightInit(self):
-        # save for reference
-        print("----------- LIMELIGHT -----------")
-        addy = limelight.discover_limelights()[0]
-        self.limelight = ll = limelight.Limelight(addy)
-        result = ll.get_results()
-        print(result)
-        print("-----------")
-        parsed_result = limelightresults.parse_results(result)
-        
-        for result in parsed_result.fiducialResults:
-            print("fiducial_id")
-            print(result.fiducial_id)
-        print("----------- LIMELIGHT -----------")
 
-    def autonomousInit(self):
+class MyRobot(commands2.TimedCommandRobot):
+    """
+    Command v2 robots are encouraged to inherit from TimedCommandRobot, which
+    has an implementation of robotPeriodic which runs the scheduler for you
+    """
+
+    autonomousCommand: typing.Optional[commands2.Command] = None
+
+    def robotInit(self) -> None:
+        """
+        This function is run when the robot is first started up and should be used for any
+        initialization code.
+        """
+
+        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        # autonomous chooser on the dashboard.
+        self.container = RobotContainer()
+
+    def robotPeriodic(self) -> None:
+        """This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+        that you want ran during disabled, autonomous, teleoperated and test.
+
+        This runs after the mode specific periodic functions, but before LiveWindow and
+        SmartDashboard integrated updating."""
+
+        # Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+        # commands, running already-scheduled commands, removing finished or interrupted commands,
+        # and running subsystem periodic() methods.  This must be called from the robot's periodic
+        # block in order for anything in the Command-based framework to work.
+        commands2.CommandScheduler.getInstance().run()
+
+    def disabledInit(self) -> None:
+        """This function is called once each time the robot enters Disabled mode."""
         pass
 
-    def autonomousPeriodic(self):
+    def disabledPeriodic(self) -> None:
+        """This function is called periodically when disabled"""
         pass
 
-    def disabledInit(self):
-        self.logger.info("%d loops / %f seconds", self.loops, self.timer.get())
+    def autonomousInit(self) -> None:
+        """This autonomous runs the autonomous command selected by your RobotContainer class."""
+        self.autonomousCommand = self.container.getAutonomousCommand()
 
-    def disabledPeriodic(self):
+        if self.autonomousCommand:
+            self.autonomousCommand.schedule()
+
+    def autonomousPeriodic(self) -> None:
+        """This function is called periodically during autonomous"""
         pass
 
-    def teleopInit (self):
-        self.loops = 0
-        self.timer.reset()
-        self.timer.start()
+    def teleopInit(self) -> None:
+        # This makes sure that the autonomous stops running when
+        # teleop starts running. If you want the autonomous to
+        # continue until interrupted by another command, remove
+        # this line or comment it out.
+        if self.autonomousCommand:
+            self.autonomousCommand.cancel()
 
-    def teleopPeriodic (self):
-        if self.limelight != None:
-            status = self.limelight.get_status()
-            result = self.limelight.get_results()
-            parsed_result = limelightresults.parse_results(result)
-            for fiducial_result in parsed_result.fiducialResults:
-                print(f"fiducial_id: {fiducial_result.fiducial_id}, cpu: {status['cpu']}")
+    def teleopPeriodic(self) -> None:
+        """This function is called periodically during operator control"""
+        pass
+
+    def testInit(self) -> None:
+        # Cancels all running commands at the start of test mode
+        commands2.CommandScheduler.getInstance().cancelAll()
